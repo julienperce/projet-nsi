81da1ad76f65923746dd0f98f49ff831fedb9fe0
julienperce <58319391+julienperce@users.noreply.github.com>
2020-11-05 23:29:16 -0600

    Update filters.js
    debugging, to further locate problem

--- js/filters.js
+++ js/filters.js
@@ -26,14 +26,16 @@ function grayscale() {
     var imgData = ctx.getImageData(0, 0, this.width, this.height),
         pxData = imgData.data, // Returns the pixel data, an array of integers 0-255
         length = pxData.length; // Returns the length of this array
-    const originalPxData = imgData.data;
-        function editImg() {
+    
+
+    function editImg() {
+
+        let opacityDiv = opacity / 100;
         for (var x = 0; x < length; x+=4) {
-            var r  = originalPxData[x], // These search for the r g b values of a pixel 
-                g = originalPxData[x + 1], // r is the 1st value of pixel1, g is the 2nd value of pixel1...
-                b = originalPxData[x + 2];
+            var r  = pxData[x], // These search for the r g b values of a pixel 
+                g = pxData[x + 1], // r is the 1st value of pixel1, g is the 2nd value of pixel1...
+                b = pxData[x + 2];
                 //default values = grey = r * 0.3 + g * 0.59 + b * 0.11;
-                let opacityDiv = opacity / 100;
                 var rMult = 0.3 * opacityDiv,
                     gMult = 0.59 * opacityDiv,
                     bMult = 0.11 * opacityDiv;
@@ -42,13 +44,17 @@ function grayscale() {
             pxData[x] = grey; // Set this new gray pixel color value back onto the existing r g b values of the pixels
             pxData[x + 1] = grey;
             pxData[x + 2] = grey;
+
         }
     }
+
     function updateImg() { 
+        let opacityDiv = opacity / 100;
         editImg();
         ctx.putImageData(imgData, 0, 0); // Paints data from our imgData object onto our canvas
-        console.log("img edited")
+        console.log("img edited", opacityDiv, pxData[0], pxData[1], pxData[2]);
     }   
+    
     slider.onchange = function() {
         text.innerHTML = this.value;
         opacity = this.value;
